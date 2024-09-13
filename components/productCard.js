64d6e46c00929bbd95fa80358dcf8cd8e86e8ebb
tsaimogono <156794044+tsaimogono@users.noreply.github.com>
2024-09-13 15:32:15 +0200

    fixed products rating

--- components/productCard.js
+++ components/productCard.js
@@ -1,20 +1,41 @@
-// app/components/ProductCard.js
 import Link from 'next/link';
 import { useState } from 'react';
 
 /**
  * ProductCard Component
- * Displays product information such as title, price, category, and image. 
- * Includes image carousel functionality and a link to the product details page.
+ * Displays product information such as title, price, category, image, and rating.
  * 
  * @param {Object} props - The component props.
  * @param {Object} props.product - The product object containing details like title, price, images, and category.
  * 
  * @returns {JSX.Element} The ProductCard component.
  */
-
 const ProductCard = ({ product }) => {
   const [currentImageIndex, setCurrentImageIndex] = useState(0);
+
+  /**
+   * Safely calculate the product's rating (rounded), fallback to 0 if product.rating or product.rating.rate is undefined.
+   */
+  const ratingValue = product.rating ? Math.round(product.rating) : 0;
+
+  // Generate star elements based on the rating value
+  const stars = Array(ratingValue).fill().map((_, i) => (
+    <svg
+      key={i}
+      xmlns="http://www.w3.org/2000/svg"
+      fill="currentColor"
+      viewBox="0 0 24 24"
+      stroke="currentColor"
+      className="w-4 h-4 text-yellow-500"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth={2}
+        d="M12 2l3.09 6.26L22 9.27l-5 4.87L18.18 21 12 17.27 5.82 21 7 14.14l-5-4.87 6.91-1.01L12 2z"
+      />
+    </svg>
+  ));
 
   /**
    * Handle switching to the next image in the product's image array.
@@ -25,6 +46,9 @@ const ProductCard = ({ product }) => {
     }
   };
 
+  /**
+   * Handle switching to the previous image in the product's image array.
+   */
   const prevImage = () => {
     if (currentImageIndex > 0) {
       setCurrentImageIndex(currentImageIndex - 1);
@@ -61,6 +85,13 @@ const ProductCard = ({ product }) => {
       <h2 className="text-lg font-bold mt-2">{product.title}</h2>
       <p className="text-purple-900 font-bold">${product.price}</p>
       <p className="text-gray-800">{product.category}</p>
+
+      {/* Rating Display */}
+      <div className="flex items-center mt-2">
+        {stars}
+        <span className="ml-2 text-gray-600">{product.rating.toFixed(1) || 0}</span>
+      </div>
+
       <Link href={`/product/${product.id}`}>
         <button className="bg-gray-500 text-white py-2 px-4 mt-2 rounded-lg transition duration-300 hover:bg-blue-300 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-50">
           View Details
